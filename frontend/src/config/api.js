10bb0f7f9643176580a03f8e54edeea0e2882316
Haris Khan <harislif3@gmail.com>
2026-01-05 00:22:31 +0500

    fix: change api base url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api'
+const API_BASE_URL = 'https://task-manager-three-lime-64.vercel.app/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
